bb6fd1e7e449020592dd473dfcae668eca38b015
suprajagantena <suprajagantena@gmail.com>
2020-04-27 13:52:33 -0400

    Update sgtest.tf

--- sgtest.tf
+++ sgtest.tf
@@ -1,1 +1,29 @@
-
+resource "aws_security_group" "new" {
+    vpc_id = aws_vpc.dev.id
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "tcp" #-1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    } 
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+   
+}
+    }
